ba12143eecebc07b30700a15a8aab9bc4bb9fe92
DarthRelkew <chancer99welker@gmail.com>
2025-01-18 23:03:27 -0700

    I did all the prep outcomes for Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,59 @@
 using System;
+using System.IO.Compression;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+    int x;
+    x = 4;
+    Console.WriteLine($"What is the magic number {x}");
+    Console.Write("What is the your guess? ");
+    string guess = Console.ReadLine();
+    int y = int.Parse(guess); 
+    
+     if(x == y)
+     {
+        Console.WriteLine("You guessed the magic number!");
+     }
+     if (x > y)
+     {
+        Console.WriteLine("Higher");
+     }
+     if (x < y)
+     {
+        Console.WriteLine("Lower");
+     }
+// Second part of the code below
+    Random randomGenerator = new Random();
+    int number = randomGenerator.Next(1,101);
+    int guess_1 = -1;
+    Console.WriteLine($"What is the magic number? {number}");
+    while (guess_1 != number)
+    {
+        Console.WriteLine("What is your guess? ");
+        string guess_3 = Console.ReadLine();
+        if(int.TryParse(guess_3, out int z))
+        {
+            if (z == number)
+            {
+                Console.WriteLine("You guessed the magic number!");
+                break;
+            }
+            if (number < z)
+            {
+                Console.WriteLine("Lower");
+            }
+            if (number > z)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+
+
+
+    }
+    
     }
 }
