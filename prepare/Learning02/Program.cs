e3867d9902c1644aec366c254aaf1bd25ed84363
Chance Edwards <chancer99welker@gmail.com>
2025-01-28 16:01:52 -0700

    This was done in class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Threading.Tasks.Dataflow;
+using Microsoft.Win32.SafeHandles;
 
 class Program
 {
+    static void DisplayMenu()
+    {
+        Console.WriteLine("Menus:");
+        Console.WriteLine("1. Add new job: ");
+        Console.WriteLine("2. Display Reume: ");
+        Console.WriteLine("3. Quit");
+        Console.WriteLine();
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Resume theResume 
+        bool keepRunning = true;
+
+        while(keepRunning)
+        {
+            DisplayMenu();
+
+            Console.WriteLine("Enter choice: ");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                // Add a new job
+                Console.Write("What is the name of the company?");
+                string company = Console.ReadLine();
+
+                Console.Write("What is the position? ");
+                string position = Console.ReadLine();
+
+                Job newJob = new Job();
+                newJob._company = company;
+            }
+            else if (choice == "2")
+            {
+                // Display Resume
+            }
+            else if (choice == "3")
+            {
+                // Quit
+                keepRunning = false;
+            }
+            else
+            {
+                //unexpected input
+                Console.WriteLine("Sorry, that is not valid. Please try again.");
+            }
+        }
     }
 }
