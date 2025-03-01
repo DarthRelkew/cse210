3820f0fa7ee788cf59c43f604548056b80c0af49
Chance Edwards <chancer99welker@gmail.com>
2025-03-01 11:35:43 -0700

    Sync test

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,12 @@
 using System;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        
+        Breathing breather = new Breathing();
+        
     }
 }
