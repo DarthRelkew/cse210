2581bdf49018045475680ef28f9a61b718778d2d
DarthRelkew <chancer99welker@gmail.com>
2025-03-01 11:20:08 -0700

    Syncing changes

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,18 @@
+public class mathAssignment : Assignment
+{
+    
+    private string _textbookSection;
+    private string _problems;
+
+    public mathAssignment(string studentName, string topic, string textbookSelection, string problems)
+        : base(studentName, topic)
+    {
+        _textbookSection = textbookSelection;
+        _problems = problems;
+
+    }
+    public string GetHomeworkList()
+    {
+        return $"Section {_textbookSection} Problems {_problems}"
+    }
+}
