ce4508c0abdf035f58227d0ec4fa78b7e7bc9123
DarthRelkew <chancer99welker@gmail.com>
2025-03-01 21:29:40 -0700

    I have the code written, but errors are showing up that I cannot pinpoint or fix. I really don't know what to do. Other than that it is all here.

--- prove/Develop04/Breathing.cs
+++ prove/Develop04/Breathing.cs
@@ -1,7 +1,16 @@
+using System.Diagnostics;
+
 public class Breathing : Activity
 {
-    public void Run()
-    {
+    public Breathing(int duration) : base("Breathing", "You will begin to relax by letting us walk you through this activity and breathing in and out.", duration) {}
 
+    protected void ShowPrompt(int index)
+    {
+        if (index % 2 == 1)
+            Console.WriteLine("Breathe in...");
+        else
+            Console.WriteLine("Breathe out...");
+        ShowAnimation();
+        Thread.Sleep(1000);
     }
 }
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,12 +1,58 @@
-using System;
-using System.IO;
-
 class Program
 {
     static void Main(string[] args)
     {
-        
-        Breathing breather = new Breathing();
-        
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Relaxation Activities Program");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflection Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Exit");
+            Console.Write("Choose and Activity (1-4): ");
+
+            string choice = Console.ReadLine();
+
+            if (choice == "1" || choice == "2" || choice == "3")
+            {
+                Console.Write("Enter the duation of the activity in seconds: ");
+                if (int.TryParse(Console.ReadLine(), out int duration) && duration >0)
+                {
+                    if (choice == "1")
+                    {
+                        Breathing breathing = new Breathing(duration);
+                        breathing.ActivityStart();
+                    }
+                    else if (choice == "2")
+                    {
+                        Reflecting reflection = new Reflecting(duration);
+                        reflection.ActivityStart();
+                    }
+                    else
+                    {
+                        Listening listening = new Listening(duration);
+                        List<string> items = listening.ListItems();
+                        Console.WriteLine($"You made a list of these items: {items.Count}");
+                        listening.ActivityStart();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("That duratin will not work, Please enter a positive number.");
+                    Thread.Sleep(2000);
+                }
+            }
+            else if (choice == "4")
+            {
+                Console.WriteLine("Thanks for playing and I hope you feel better soon.");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Please choose an activity between 1-4.");
+                Thread.Sleep(2000);
+            }
+        }
     }
 }
